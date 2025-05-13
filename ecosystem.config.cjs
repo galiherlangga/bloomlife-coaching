4fd2bc6501399dc2f9ddc289a8f6d668e2b691d7
Galih Erlangga <galiherlangga666@gmail.com>
2025-05-13 13:56:03 +0800

    Update ecosystem configuration to use Node as the interpreter and set PORT to 3003

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,12 +2,11 @@ module.exports = {
   apps: [
     {
       name: "bloomlife",
-      script: "build/index.js",   // your built output
-      interpreter: "bun",         // bun is the runtime
-      interpreter_args: "run",    // tells bun to actually run it
+      script: "build/index.js",
+      interpreter: "node",
       env: {
         NODE_ENV: "production",
-        PORT: 3000
+        PORT: 3003
       }
     }
   ]
